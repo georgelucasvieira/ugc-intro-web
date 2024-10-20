3c71f99ae21365d0f4e00d7993d5cb9552140398
georgelucasvieira <george.vieira.carmo@gmail.com>
2024-10-20 20:19:08 -0300

    fix: repository reference name

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -19,7 +19,7 @@
       </div>
     
   
-      <button on:click|preventDefault={() => window.location.href = selectedOption === 'search-query' ? '/ugc-web-intro/search-query' : '/ugc-web-intro/search-link'}>
+      <button on:click|preventDefault={() => window.location.href = selectedOption === 'search-query' ? '/ugc-intro-web/search-query' : '/ugc-intro-web/search-link'}>
         Search
       </button>
   
