05509cd8b7a9859ac2d2516ed18e772b0239c9c2
georgelucasvieira <george.vieira.carmo@gmail.com>
2024-10-20 20:19:33 -0300

    Updates

--- assets/loop-BiPzcsH1.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./writeAndUnwriteText-CWBezCIH.js";import{a as s,m}from"./animationSetup-CsugjxlP.js";import"./writeEffect-C9avOEay.js";import"./unwriteEffect-D9-UgXKG.js";import"./index-CGrMYlNr.js";const u=async(t,e)=>{const{options:o,elements:i}=s(t,e);for(;;){m(t);for(const r of i)await a(r,o)}return{update(){},destroy(){}}};export{u as default};
